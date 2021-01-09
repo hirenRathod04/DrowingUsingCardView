8dce05eb64c872c8d6ab68a5b372aac759475f8c
Hiren <rs.hirenrathod@gmail.com>
2021-01-09 17:17:12 +0530

    Initial commit

---
+++ app/src/main/java/com/royalsoftsolutions/drowingusingcardview/CardActivity2.java
@@ -0,0 +1,14 @@
+package com.royalsoftsolutions.drowingusingcardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card2);
+    }
+}
